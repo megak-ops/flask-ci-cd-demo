0f4a966a778794bb64b88396240797af8efa935b
megak-ops <khmegally@gmail.com>
2025-12-06 02:44:59 +0200

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,8 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello from Flask CI/CD Pipeline!"
+    return "Hello, world!"
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    # run server on all interfaces عشان Docker يقدر يوصله
+    app.run(host="0.0.0.0", port=5000, debug=True)
